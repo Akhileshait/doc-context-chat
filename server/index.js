a5bb1e4d04f87102bdc4617ca34dea3c9fffb08a
Akhilesh Patidar <akhileshpatidar989368@gmail.com>
2025-06-15 14:08:38 +0530

    vector embeddings done with gemini

--- server/index.js
+++ server/index.js
@@ -1,44 +1,98 @@
-import express from 'express';
-import cors from 'cors';
-import multer from 'multer';
-import {Queue} from 'bullmq';
+import express from "express";
+import cors from "cors";
+import multer from "multer";
+import { Queue } from "bullmq";
+import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+import { QdrantVectorStore } from "@langchain/qdrant";
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import OpenAI from "openai";
 
 const queue = new Queue("file-upload");
+
+// hitting the gemini API using OpenAI client
+const client = new OpenAI({
+  apiKey: "AIzaSyB2BqN_HUBqew-c3ACUKx9hhflH4-aE1T0",
+  baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/",
+});
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(cors());    
+app.use(cors());
 app.use(express.json());
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/')
+    cb(null, "uploads/");
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, `${uniqueSuffix}-${file.originalname}`)
-  }
-})
-
-const upload = multer({storage: storage});
-
-
-
-app.get('/', (req, res) => {
-  res.send('Welcome to the API PDF CHAT!');
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, `${uniqueSuffix}-${file.originalname}`);
+  },
 });
 
-app.post('/upload/pdf', upload.single('pdf'), (req, res) => {
-    queue.add('file-ready', JSON.stringify({
+const upload = multer({ storage: storage });
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the API PDF CHAT!");
+});
+
+app.post("/upload/pdf", upload.single("pdf"), (req, res) => {
+  queue.add(
+    "file-ready",
+    JSON.stringify({
       filename: req.file.originalname,
       destination: req.file.destination,
       path: req.file.path,
-    }));
+    })
+  );
 
-    res.json({
-        message: 'PDF uploaded successfully'
-    });
+  res.json({
+    message: "PDF uploaded successfully",
+  });
+});
+
+app.get("/chat", async (req, res) => {
+  const userQuery = "What is react in detail?";
+
+  const embeddings = new GoogleGenerativeAIEmbeddings({
+    model: "text-embedding-004", // 768 dimensions
+    apiKey: "AIzaSyB2BqN_HUBqew-c3ACUKx9hhflH4-aE1T0",
+  });
+
+  const vectorStore = await QdrantVectorStore.fromExistingCollection(
+    embeddings,
+    {
+      url: "http://localhost:6333",
+      collectionName: "pdf-docs",
+    }
+  );
+
+  const ret = vectorStore.asRetriever({ k: 2 });
+  const result = await ret.invoke(userQuery);
+
+  const SYSTEM_PROMPT = `You are a helpful assistant. You will be provided with context from a PDF document. Use this context to answer the user's question. ${JSON.stringify(
+    result
+  )}`;
+
+  const chatResponse = await client.chat.completions.create({
+    model: "gemini-2.5-flash-preview-05-20",
+    messages: [
+      {
+        role: "system",
+        content: SYSTEM_PROMPT,
+      },
+      {
+        role: "user",
+        content: userQuery,
+      },
+    ],
+  });
+
+  return res.json({
+    message: chatResponse.choices[0].message.content,
+    docs: result,
+  });
 });
 
 app.listen(PORT, () => {
--- server/worker.js
+++ server/worker.js
@@ -1,5 +1,55 @@
 import { Worker } from 'bullmq';
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { QdrantVectorStore } from "@langchain/qdrant";
+import { Document } from "@langchain/core/documents";
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { CharacterTextSplitter } from "@langchain/textsplitters";
+import { QdrantClient } from "@qdrant/js-client-rest";
+import { ChatOpenAI } from "@langchain/openai";
+import { GoogleGenerativeAIEmbeddings } from '@langchain/google-genai';
+
+const key = "AIzaSyB2BqN_HUBqew-c3ACUKx9hhflH4-aE1T0";
 
 const worker = new Worker('file-upload', async (job) => {
-  console.log(`Processing job ${job.id} with data:`, job.data);
+    console.log(`Processing job ${job.id} with data:`, job.data);
+    const data = JSON.parse(job.data);
+
+    //Load the PDF file
+    const loader = new PDFLoader(data.path);
+    const docs = await loader.load();
+
+    
+
+    // Split the documents into chunks
+    // const textSplitter = new CharacterTextSplitter({
+    // chunkSize: 300,
+    // chunkOverlap: 0,
+    // });
+    // const texts = await textSplitter.splitText(docs);
+    // console.log(texts);
+    
+    // Create Vector embeddings from chunked documents
+
+
+    const embeddings = new GoogleGenerativeAIEmbeddings({
+        model: "text-embedding-004", // 768 dimensions
+        apiKey: key,
+    });
+
+    console.log(embeddings);
+    
+
+    console.log(`Creating vector embeddings for documents...`);
+    
+
+    const vectorStore = await QdrantVectorStore.fromExistingCollection(embeddings, {
+        url: 'http://localhost:6333',
+        collectionName: "pdf-docs",
+    });
+
+    await vectorStore.addDocuments(docs);
+
+    console.log(`Document vector store embeddings processed successfully.`);
+
+
 }, { concurrency: 100, connection: { host: 'localhost', port: 6379 } });
